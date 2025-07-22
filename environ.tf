5152e5bff9fc920ae84a42b1f3f25cdba23efdb8
Pratik <thisguy@pratikthakare.com>
2025-07-23 04:47:04 +0530

    feat: added environ.tf
    added bucket sub emails for aws-infra stack

---
+++ environ.tf
@@ -0,0 +1,8 @@
+resource "spacelift_environment_variable" "aws_infra_bse" {
+  stack_id    = "aws-infra"
+  name        = "TF_VAR_budget_subscription_emails"
+  value       = jsonencode(["aws@pratikthakare.com","patrick.thakare123@gmail.com"])
+  write_only  = false
+  description = "List of emails to which budget notifications will be sent"
+}
+
