57181afeefc66279360b02d53ab4402cd11bb004
Pratik <thisguy@pratikthakare.com>
2025-07-23 04:16:01 +0530

    feat: added aws-infra stack

--- stack.tf
+++ stack.tf
@@ -15,3 +15,21 @@ resource "spacelift_stack" "spacelift-iac" {
   enable_sensitive_outputs_upload  = false
   github_action_deploy             = false
 }
+
+resource "spacelift_stack" "aws-infra" {
+  name        = "aws-infra"
+  description = "Manages the AWS account of aws@pratikthakare.com"
+  space_id    = "root"
+
+  repository = "aws-infra"
+  branch     = "main"
+
+  terraform_version            = "1.10.3"
+  terraform_workflow_tool      = "OPEN_TOFU"
+  terraform_smart_sanitization = true
+
+  administrative                   = false
+  enable_well_known_secret_masking = true
+  enable_sensitive_outputs_upload  = false
+  github_action_deploy             = false
+}
