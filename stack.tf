d6afc03b4fc34b5d79964a72f3951c7499fcab93
Pratik <thisguy@pratikthakare.com>
2025-07-23 04:01:54 +0530

    feat: followed dashboard defaults
    set enable_sensitive_outputs_upload as false and added description

--- stack.tf
+++ stack.tf
@@ -1,15 +1,17 @@
 resource "spacelift_stack" "spacelift-iac" {
-  name = "spacelift-iac"
-  space_id = "root"
+  name        = "spacelift-iac"
+  description = "Manages this Spacelift organization and objects within"
+  space_id    = "root"
 
   repository = "spacelift-iac"
-  branch = "main"
+  branch     = "main"
 
-  terraform_version = "1.10.3"
-  terraform_workflow_tool = "OPEN_TOFU"
+  terraform_version            = "1.10.3"
+  terraform_workflow_tool      = "OPEN_TOFU"
   terraform_smart_sanitization = true
 
-  administrative = true
+  administrative                   = true
   enable_well_known_secret_masking = true
-  github_action_deploy = false
+  enable_sensitive_outputs_upload  = false
+  github_action_deploy             = false
 }
